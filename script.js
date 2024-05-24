aea4dd2a71f4d29ec02991cbdc7f25cd7bee53e3
gangmin2 <min0115b@gmail.com>
2024-05-24 10:33:07 +0900

    변수 선언 및 할당

---
+++ script.js
@@ -0,0 +1,27 @@
+const $result = document.querySelector('#result');
+
+const $clear = document.querySelector('#clear');
+const $plusmn = document.querySelector('#plusmn');
+const $percent = document.querySelector('#percent');
+
+const $divide = document.querySelector('#divide');
+const $multiply = document.querySelector('#multiply');
+const $minus = document.querySelector('#minus');
+const $plus = document.querySelector('#plus');
+const $calculate = document.querySelector('#calculate');
+
+const $num0 = document.querySelector('#num-0');
+const $num1 = document.querySelector('#num-1');
+const $num2 = document.querySelector('#num-2');
+const $num3 = document.querySelector('#num-3');
+const $num4 = document.querySelector('#num-4');
+const $num5 = document.querySelector('#num-5');
+const $num6 = document.querySelector('#num-6');
+const $num7 = document.querySelector('#num-7');
+const $num8 = document.querySelector('#num-8');
+const $num9 = document.querySelector('#num-9');
+const $dot = document.querySelector('#dot');
+
+let operand1 = '';
+let operand2 = '';
+let operator = '';
